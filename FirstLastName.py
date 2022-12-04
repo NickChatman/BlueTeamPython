4e6632ee96ceac203d3b0a5e59b7198320910dc4
NickChatman <115037298+NickChatman@users.noreply.github.com>
2022-12-04 17:12:52 -0600

    Create FirstLastName.py

---
+++ FirstLastName.py
@@ -0,0 +1,9 @@
+def print_full_name(first, last):
+#Intalize Your First and Last Name
+first:"#YourFirstName" #First Name
+last:"#YourLastName"   #Last Name
+string: 'Hello first last' #String Function That combines your First and Last Name
+if __name__ == '__main__': #Condition
+first_name = input() #Input
+last_name = input() #Input
+print_full_name(first_name, last_name) # Will print first and last name
